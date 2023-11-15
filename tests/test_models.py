4a26127882b372c7aa0be1cdbb475bc0d7f07a47
yunji0387 <yunji0387@gmail.com>
2023-11-14 22:17:56 -0500

    increase test coverage of test cases for Models

--- tests/test_models.py
+++ tests/test_models.py
@@ -200,3 +200,15 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.category, category)
+
+    def test_find_by_price(self):
+        """It should Find Products by Price"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        price = products[0].price
+        count = len([product for product in products if product.price == price])
+        found = Product.find_by_price(price)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.price, price)
